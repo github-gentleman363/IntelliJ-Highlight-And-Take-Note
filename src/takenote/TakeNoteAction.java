63f1ff7c2302770b59367560eb94801bf35f9636
Jiwoong Youn <jiwoong.youn@appdynamics.com>
2017-04-16 22:46:11 -0700

    Refactor TakeNoteAction and add IconRenderer

--- src/takenote/TakeNoteAction.java
+++ src/takenote/TakeNoteAction.java
@@ -3,20 +3,16 @@ package takenote;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.SelectionModel;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.FileEditorManagerAdapter;
-import com.intellij.openapi.fileEditor.FileEditorManagerListener;
+import com.intellij.openapi.fileEditor.*;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.messages.MessageBus;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
-
 
 public class TakeNoteAction extends AnAction {
 
@@ -25,28 +21,21 @@ public class TakeNoteAction extends AnAction {
         final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
         final Project project = e.getRequiredData(CommonDataKeys.PROJECT);
 
-        MessageBus messageBus = project.getMessageBus();
-        messageBus.connect().subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, new FileEditorManagerAdapter() {
-            @Override
-            public void fileOpened(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
-                super.fileOpened(source, file);
-                NoteGutter noteGutter = new NoteGutter(project, source.getSelectedTextEditor());
-                source.getSelectedTextEditor().getGutter().registerTextAnnotation(noteGutter, noteGutter);
-            }
-        });
+        this.subscribeRegisterNotesOnFileOpened(project);
 
         final SelectionModel selectionModel = editor.getSelectionModel();
         final int startOffset = selectionModel.getSelectionStart();
         final int endOffset = selectionModel.getSelectionEnd();
         if (selectionModel.getSelectionStartPosition() == null) return;
         final int lineNumber = selectionModel.getSelectionStartPosition().getLine();
+        final String code = selectionModel.getSelectedText();
 
-        String code = selectionModel.getSelectedText();
-        Document document = editor.getDocument();
-        VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(document);
+        final VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(editor.getDocument());
         if (virtualFile == null) return;
 
         final String filePath = virtualFile.getPath();
+        // TODO: use the code below if we want path to be relative to base directory
+        // final String filePath = VfsUtil.getRelativeLocation(virtualFile, project.getBaseDir());
 
         // TODO set position & size
         TakeNoteDialogWrapper dialogWrapper = new TakeNoteDialogWrapper(project, true);
@@ -61,6 +50,21 @@ public class TakeNoteAction extends AnAction {
 
             manager.addNewNote(startOffset, endOffset, lineNumber, comment, filePath, code, color);
 
+            /* TODO: get rid of text annotation and use icon renderer
+            OpenFileDescriptor openFileDescriptor = getOpenFileDescriptor(project, newNote.getFilePath(),
+                    newNote.getStartOffset());
+            if (openFileDescriptor == null) return;
+            final Editor editor1 = FileEditorManagerEx.getInstance(project).
+                    openTextEditor(openFileDescriptor, true);
+            if (editor1 == null) return;
+            MarkupModelEx markup = (MarkupModelEx) editor1.getMarkupModel();
+            RangeHighlighter highlighter = markup.addPersistentLineHighlighter(newNote.getLineNumber(), HighlighterLayer.ERROR + 1,
+                    null);
+            if(highlighter == null) return;
+            GutterIconRenderer gutterIconRenderer = new NoteGutterIconRenderer();
+            highlighter.setGutterIconRenderer(gutterIconRenderer);
+            */
+
             // TODO fix duplicate annotation
             // TODO display color / icon next to where note is taken
             //      AND add action logic
@@ -70,5 +74,34 @@ public class TakeNoteAction extends AnAction {
             NoteGutter noteGutter = new NoteGutter(project, editor);
             editor.getGutter().registerTextAnnotation(noteGutter, noteGutter);
         }
+    }
+
+    /* TODO: methods needed for icon renderer
+    private OpenFileDescriptor getOpenFileDescriptor(Project project, String filePath, int offset) {
+        final VirtualFile virtualFile = getVirtualFile(project, filePath);
+        if (virtualFile == null) return null;
+        return new OpenFileDescriptor(project, virtualFile, offset);
+    }
+
+    private VirtualFile getVirtualFile(Project project, String filePath) {
+        VirtualFile baseDir = project.getBaseDir();
+        System.out.println("here");
+        if(baseDir == null)  {return null;}
+        System.out.println("there");
+        return baseDir.findFileByRelativePath(filePath);
+    }
+    */
+
+    private void subscribeRegisterNotesOnFileOpened(Project project) {
+        MessageBus messageBus = project.getMessageBus();
+        messageBus.connect().subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, new FileEditorManagerAdapter() {
+            @Override
+            public void fileOpened(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
+                super.fileOpened(source, file);
+                if (source.getSelectedTextEditor() == null) return;
+                NoteGutter noteGutter = new NoteGutter(project, source.getSelectedTextEditor());
+                source.getSelectedTextEditor().getGutter().registerTextAnnotation(noteGutter, noteGutter);
+            }
+        });
     }
 }
---
+++ src/takenote/gutter/NoteGutterIconRenderer.java
@@ -0,0 +1,32 @@
+package takenote.gutter;
+
+import com.intellij.openapi.editor.markup.GutterIconRenderer;
+import com.intellij.openapi.util.IconLoader;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class NoteGutterIconRenderer extends GutterIconRenderer {
+    private final Icon icon = IconLoader.getIcon("/takenote/images/note.png");
+
+
+    @NotNull
+    @Override
+    public Icon getIcon() {
+        return icon;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NoteGutterIconRenderer that = (NoteGutterIconRenderer) o;
+        return icon.equals(that.getIcon());
+    }
+
+    @Override
+    public int hashCode() {
+        return getIcon().hashCode();
+    }
+
+}
